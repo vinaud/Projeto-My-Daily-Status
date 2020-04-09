fd3225ce4b6f4484019aee3ce1134b9989828014
João Vinaud <leyenasd@gmail.com>
2020-04-08 23:41:11 -0300

    commit inicial

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className = 'py-4 text-center bg-gray-400'>
+            My Daily Status é um projeto criado durante o Fullstack Lab do Dev Pleno <br/>
+            Implementado por : {''} 
+            <a href = 'https://www.linkedin.com/in/joao-vinaud/'>João Vinaud</a>
+        </div>
+    )
+};
+
+export default Footer;
---
+++ components/NavBar.js
@@ -0,0 +1,23 @@
+import React, { Children } from 'react';
+import Link from 'next/link';
+
+const NavLink = ({href, children}) =>{
+    return(
+        <Link href = {href} >
+            <a className = 'p-2 hover:underline hover:text-red-800'>{children}</a>
+        </Link>
+    )
+}
+const NavBar = () =>{
+    return (
+        <div className='bg-gray-500 py-4 text-center'>
+            <NavLink href = '/sobre' > Sobre </NavLink>
+               
+            <NavLink href = '/cadastro'>Cadastre-se</NavLink>
+
+            <NavLink href = '/entrar'><a>Entrar</a></NavLink>
+       </div>
+    )
+}
+
+export default NavBar;
