9bb1f70b68e441dc44c7fd687bb01f97eea6caa4
João Vinaud <leyenasd@gmail.com>
2020-04-09 23:15:25 -0300

    sessão do usuarioo

---
+++ pages/app.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import auth0 from '../lib/auth0';
+
+const App = (props) => {
+
+    return (
+        <div>
+            <h1>App</h1>
+            <pre>{JSON.stringify(props, null, 2)}</pre>
+        </div>
+        
+    )
+}
+
+export default App;
+
+export async function getServerSideProps({ req, res })  {
+    const session = await auth0.getSession(req);
+    console.log(session)
+    if(session){
+        return {
+           props: {
+               user: session.user
+           }
+        }
+    }
+    return {
+        props: {
+            user: { }
+        }
+    }
+}
