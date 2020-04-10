2f9d01b0bae392941fc15b709a6d77d8a8c35e6f
João Vinaud <leyenasd@gmail.com>
2020-04-09 22:50:26 -0300

    botão de login no home

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,7 @@
 import auth0 from '../../lib/auth0';
 
-const login = async(request, response) => {
-    auth0.handleLogin()
+const login = async (request, response) => {
+    await auth0.handleLogin(request, response)
     response.send({
         name: 'joão vinaud'
     })
