db2877a5e0174181f6755c9d0cf48096fcb1cd10
João Vinaud <leyenasd@gmail.com>
2020-04-10 23:58:40 -0300

    limpeza de codigo

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,9 +2,7 @@ import auth0 from '../../lib/auth0';
 
 const login = async (request, response) => {
     await auth0.handleLogin(request, response)
-    response.send({
-        name: 'joão vinaud'
-    })
+    
 }
 
 export default login;
