5bd1d5b4dea57a8950cf4afc038126ed6ce1a285
João Vinaud <leyenasd@gmail.com>
2020-04-12 18:31:52 -0300

    texto da página 'sobre'

--- pages/sobre.js
+++ pages/sobre.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Sobre = () => {
     return(
-        <h1>Sobre</h1>
+        <div>
+            <h1 className = 'text-3xl text-center m-8'>Uma forma de ajudar o controle da doença é saber onde ela está.</h1>
+            <h1 className = 'text-3xl text-center m-8'>E se você soubesse como as pessoas ao seu redor estivessem?</h1>
+            <h1 className = 'text-3xl text-center m-8'>Você compartilha como você  está hoje, e só assim consegue visualizar as pessoas a sua volta de forma anônima.</h1>
+        </div>
     )
 };
 
